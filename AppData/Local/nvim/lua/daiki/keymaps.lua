c343fa8bd9fd859bef0cface7f47fe0cf6d3da06
Daiki48 <daiki48.engineer@gmail.com>
2022-10-20 22:45:58 +0900

    [add] packer action

--- AppData/Local/nvim/lua/daiki/keymaps.lua
+++ AppData/Local/nvim/lua/daiki/keymaps.lua
@@ -3,3 +3,7 @@ local keymap = vim.api.nvim_set_keymap
 -- buffer moving
 keymap('n', '<C-l>', '<cmd>bnext<cr>', { noremap = true, silent = true })
 keymap('n', '<C-h>', '<cmd>bprevious<cr>', { noremap = true, silent = true })
+
+-- packer command
+keymap('n', 'ps', '<cmd>PackerSync<cr>', { noremap = true, silent = true })
+keymap('n', 'pi', '<cmd>PackerInstall<cr>', { noremap = true, silent = true })
