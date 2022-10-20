1135609bc60966ec2d3acac71227a12e97d2b1ef
Daiki48 <daiki48.engineer@gmail.com>
2022-10-20 22:47:11 +0900

    [delete] nightfox ^& lualine ^& treesitter

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -1,6 +1,3 @@
 require('daiki.base')            -- base options
 require('plugins.packerconfig')  -- plugin manager
 require('daiki.keymaps')         -- keymaps
-require('daiki.nightfox')        -- colorscheme
-require('daiki.lualine')         -- statusline
-require('treesitter.treesitter') -- treesitter config
