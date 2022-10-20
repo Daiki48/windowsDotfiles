e60b4dc928157764ea48c503f350dfe20e97449e
Daiki48 <daiki48.engineer@gmail.com>
2022-10-20 17:19:29 +0900

    [add] keymaps ^& treesitter

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -1,4 +1,6 @@
 require('daiki.base')            -- base options
 require('plugins.packerconfig')  -- plugin manager
+require('daiki.keymaps')         -- keymaps
 require('daiki.nightfox')        -- colorscheme
 require('daiki.lualine')         -- statusline
+require('treesitter.treesitter') -- treesitter config
