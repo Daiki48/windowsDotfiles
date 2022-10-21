bd9c99b5bed1fbf805e1369dd971257b7f282e65
Daiki48 <daiki48.engineer@gmail.com>
2022-10-22 00:39:45 +0900

    [add] lsp keymaps

--- AppData/Local/nvim/lua/daiki/keymaps.lua
+++ AppData/Local/nvim/lua/daiki/keymaps.lua
@@ -7,3 +7,16 @@ keymap('n', '<C-h>', '<cmd>bprevious<cr>', { noremap = true, silent = true })
 -- packer command
 keymap('n', 'ps', '<cmd>PackerSync<cr>', { noremap = true, silent = true })
 keymap('n', 'pi', '<cmd>PackerInstall<cr>', { noremap = true, silent = true })
+
+-- lsp keymaps
+local set = vim.keymap.set
+set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+set("n", "<C-m>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+set("n", "gy", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+set("n", "rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+set("n", "ma", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>")
+set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>")
+set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>")
