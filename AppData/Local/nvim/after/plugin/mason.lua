e1cdff69569c9c29fdca2cba2db2a5a67971d589
Daiki48 <daiki48.engineer@gmail.com>
2022-10-22 01:03:32 +0900

    [update]

--- AppData/Local/nvim/after/plugin/mason.lua
+++ AppData/Local/nvim/after/plugin/mason.lua
@@ -8,5 +8,20 @@ mason.setup({
 })
 
 lspconfig.setup {
-  automatic_installation = true
+	automatic_installation = true
+}
+
+-- local on_attach = function(client, bufnr)
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities(
+	vim.lsp.protocol.make_client_capabilities()
+)
+
+require("mason-lspconfig").setup_handlers {
+	function(server_name) -- default handler (optional)
+		require("lspconfig")[server_name].setup {
+			-- on_attach = on_attach, --keyバインドなどの設定を登録
+			capabilities = capabilities, --cmpを連携
+		}
+	end,
 }
