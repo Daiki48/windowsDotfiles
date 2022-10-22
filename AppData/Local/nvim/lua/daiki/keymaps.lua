be74a8ff61793e70b6655cae12f800ae1794d981
Daiki48 <daiki48.engineer@gmail.com>
2022-10-22 13:42:23 +0900

    [add] buffer delete keymap

--- AppData/Local/nvim/lua/daiki/keymaps.lua
+++ AppData/Local/nvim/lua/daiki/keymaps.lua
@@ -3,6 +3,7 @@ local keymap = vim.api.nvim_set_keymap
 -- buffer moving
 keymap('n', '<C-l>', '<cmd>bnext<cr>', { noremap = true, silent = true })
 keymap('n', '<C-h>', '<cmd>bprevious<cr>', { noremap = true, silent = true })
+keymap('n', '<C-q>', '<cmd>bd<cr>', { noremap = true })
 
 -- packer command
 keymap('n', 'ps', '<cmd>PackerSync<cr>', { noremap = true, silent = true })
