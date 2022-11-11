382ab23ef29c8948006392c93257920b03fd44e3
Daiki48 <daiki48.engineer@gmail.com>
2022-11-11 23:44:31 +0900

    [add] default plugin Disable

---
+++ AppData/Local/nvim/lua/daiki/disable.lua
@@ -0,0 +1,37 @@
+-- alias to vim's objects
+local g = vim.g
+
+-- disable default plugins
+local disable_plugins = {
+	"loaded_gzip",
+	"loaded_shada_plugin",
+	"loadedzip",
+	"loaded_spellfile_plugin",
+	"loaded_tutor_mode_plugin",
+	"loaded_gzip",
+	"loaded_tar",
+	"loaded_tarPlugin",
+	"loaded_zip",
+	"loaded_zipPlugin",
+	"loaded_rrhelper",
+	"loaded_2html_plugin",
+	"loaded_vimball",
+	"loaded_vimballPlugin",
+	"loaded_getscript",
+	"loaded_getscriptPlugin",
+	"loaded_logipat",
+	"loaded_matchparen",
+	"loaded_man",
+	"loaded_netrw",
+	"loaded_netrwPlugin",
+	"loaded_netrwSettings",
+	"loaded_netrwFileHandlers",
+	"loaded_logiPat",
+	"did_install_default_menus",
+	"did_install_syntax_menu",
+	"skip_loading_mswin",
+}
+
+for _, name in pairs(disable_plugins) do
+	g[name] = true
+end
