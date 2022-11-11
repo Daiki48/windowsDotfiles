1c7db37ed320e0e12f9184d8bc97e79daa578bfc
Daiki48 <daiki48.engineer@gmail.com>
2022-11-11 23:43:55 +0900

    [update] Enable [Completion in Tab ^& S-Tab] and Change [lspkind -^> icons]

--- AppData/Local/nvim/after/plugin/cmp.lua
+++ AppData/Local/nvim/after/plugin/cmp.lua
@@ -1,76 +1,180 @@
 local status, cmp = pcall(require, "cmp")
 if (not status) then return end
-local lspkind = require 'lspkind'
+-- local cmp = require("cmp")
 
-cmp.setup({
+-- local lspkind = require 'lspkind'
+
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then
+	return
+end
+
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+local icons = {
+	Text          = ' ',
+	Method        = ' ',
+	Function      = ' ',
+	Constructor   = ' ',
+	Field         = '識',
+	Variable      = '𝑋 ',
+	Class         = ' ',
+	Interface     = ' ',
+	Module        = ' ',
+	Property      = ' ',
+	Unit          = ' ',
+	Value         = ' ',
+	Enum          = ' ',
+	Keyword       = ' ',
+	Snippet       = '﬌ ',
+	Color         = ' ',
+	File          = ' ',
+	Reference     = ' ',
+	Folder        = ' ',
+	EnumMember    = ' ',
+	Constant      = ' ',
+	Struct        = ' ',
+	Event         = ' ',
+	Operator      = 'ﬦ',
+	TypeParameter = ' ',
+}
+
+local compare = require "cmp.config.compare"
+
+local cmp_window = require "cmp.utils.window"
+
+cmp_window.info_ = cmp_window.info
+cmp_window.info = function(self)
+	local info = self:info_()
+	info.scrollable = false
+	return info
+end
+
+local options = {
+	window = {
+		completion = {
+			winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,Search:None",
+		},
+		documentation = {
+			winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+		},
+	},
 	snippet = {
 		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
+			luasnip.lsp_expand(args.body)
 		end,
 	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.close(),
-		['<CR>'] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true
-		}),
-		['<Tab>'] = function(fallback)
+	mapping = {
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-u>"] = cmp.mapping.scroll_docs(-4),
+		["<C-d>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.close(),
+		["<CR>"] = cmp.mapping.confirm { select = true },
+		["<Down>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
 			else
 				fallback()
 			end
-		end,
-		['<S-Tab'] = function(fallback)
+		end, { "i", "s" }),
+		["<Up>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
 			else
 				fallback()
 			end
-		end,
-	}),
-	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		{ name = 'buffer' },
-		{ name = 'path' },
-		{ name = 'vsnip' },
-		{ name = 'skkeleton' },
-	}),
-	formatting = {
-		format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
+		end, { "i", "s" }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 	},
-	view = {
-		entries = 'native'
-	}
-})
-
-vim.cmd [[
-  set completeopt=menuone,noinsert,noselect
-  highlight! default link CmpItemKind CmpItemMenuDefault
-]]
-
--- Use <Tab> and <S-Tab> to navigate through popup menu
--- inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
--- inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
-
-
--- local keymap = vim.api.nvim_set_keymap
--- keymap('i', '<expr> <Tab>', 'pumvisible() ? "<C-n>" : "<Tab>"', { noremap = true, silent = true })
--- keymap('i', '<expr> <S-Tab>', 'pumvisible() ? "<C-p>" : "<S-Tab>"', { noremap = true, silent = true })
-
--- cmp.setup.cmdline('/', {
--- 	mapping = cmp.mapping.preset.cmdline(),
--- 	sources = {
--- 		{ name = 'buffer' } -- search source setting
--- 	}
--- })
-
-cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
 	sources = {
-		{ name = "path" }, -- command source setting
+		{ name = "nvim_lsp",
+			filter = function(entry, ctx)
+				local kind = require("cmp.types.lsp").CompletionItemKind[entry:get_kind()]
+				if kind == "Snippet" and ctx.prev_context.filetype == "java" then
+					return true
+				end
+
+				if kind == "Text" then
+					return false
+				end
+			end,
+			group_index = 2 },
+		{ name = "luasnip", option = { use_show_condition = false }, group_index = 2 },
+		{ name = "nvim_lua", group_index = 2 },
+		{ name = "buffer", group_index = 2 },
+		{ name = "path", group_index = 2 },
 	},
-})
+	sorting = {
+		priority_weight = 2,
+		comparators = {
+			compare.offset,
+			compare.exact,
+			compare.score,
+			compare.recently_used,
+			compare.locality,
+			compare.sort_text,
+			compare.length,
+			compare.order,
+		},
+	},
+	completion = {
+		completeopt = 'menu,menuone,noinsert',
+	},
+	formatting = {
+		fields = { "kind", "abbr", "menu" },
+		format = function(_, vim_item)
+			vim_item.kind = icons[vim_item.kind]
+			-- vim_item.ind = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
+			vim_item.menu = ({
+				nvim_lsp = "",
+				nvim_lua = "",
+				luasnip = "",
+				buffer = "",
+				path = "",
+				emoji = "",
+			})[_.source.name]
+			return vim_item
+		end,
+	},
+	confirm_opts = {
+		behavior = cmp.ConfirmBehavior.Replace,
+		select = false,
+	},
+	experimental = {
+		ghost_text = true,
+	},
+}
+
+cmp.setup(options)
