ce52b54262355f69de43a0d5719fc6da5bfa2cbd
Daiki48 <daiki48.engineer@gmail.com>
2022-11-11 23:45:33 +0900

    [add] disable.lua

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -1,3 +1,8 @@
-require('daiki.base')            -- base options
-require('plugins.packerconfig')  -- plugin manager
-require('daiki.keymaps')         -- keymaps
+require('daiki.disable') -- default plugin disable
+require('daiki.base') -- base options
+require('plugins.packerconfig') -- plugin manager
+require('daiki.keymaps') -- keymaps
+
+-- vim.opt.runtimepath:append("D:/projects/vim-plugin-dev/navigation.nvim")
+
+-- require("navigation")
