6338ea05b672a295977465315297eddbf2ed3f95
Daiki48 <daiki48.engineer@gmail.com>
2022-11-13 21:12:14 +0900

    [issue?] Not work

--- AppData/Local/nvim/after/plugin/noice.lua
+++ AppData/Local/nvim/after/plugin/noice.lua
@@ -1,4 +1,51 @@
 local status, noice = pcall(require, 'noice')
 if (not status) then return end
 
-noice.setup()
+require("telescope").load_extension("noice")
+
+local function myMiniView(pattern, kind)
+	kind = kind or ""
+	return {
+		view = "mini",
+		filter = {
+			event = "msg_show",
+			kind = kind,
+			find = pattern,
+		},
+	}
+end
+
+noice.setup({
+	messages = {
+		view_search = "mini",
+	},
+	routes = {
+		{
+			view = "notify",
+			filter = { event = "msg_showmode" },
+		},
+		{
+			filter = {
+				event = "notify",
+				warning = true,
+				find = "failed to run generator.*is not executable",
+			},
+			opts = { skip = true },
+		},
+		myMiniView("Already at .* change"),
+		myMiniView("written"),
+		myMiniView("yanked"),
+		myMiniView("more lines?"),
+		myMiniView("fewer lines?"),
+		myMiniView("fewer lines?", "lua_error"),
+		myMiniView("change; before"),
+		myMiniView("change; after"),
+		myMiniView("line less"),
+		myMiniView("lines indented"),
+		myMiniView("No lines in buffer"),
+		myMiniView("search hit .*, continuing at", "wmsg"),
+		myMiniView("E486: Pattern not found", "emsg"),
+	},
+})
+
+-- noice.setup()
