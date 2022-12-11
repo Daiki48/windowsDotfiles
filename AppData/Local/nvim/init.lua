a228157a5deda6d2cd4ca552f664a78e13fdced6
Daiki48 <daiki48.engineer@gmail.com>
2022-12-12 01:03:44 +0900

    :wrench: config update

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -1,8 +1,1 @@
-require('daiki.disable') -- default plugin disable
-require('daiki.base') -- base options
-require('plugins.packerconfig') -- plugin manager
-require('daiki.keymaps') -- keymaps
-
--- vim.opt.runtimepath:append("D:/projects/vim-plugin-dev/navigation.nvim")
-
--- require("navigation")
+require('daiki')
