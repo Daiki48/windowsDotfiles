ac8dfd8d2f706ce45c14e56648a66a187d7d6f86
Daiki48 <daiki48.engineer@gmail.com>
2022-12-12 22:26:34 +0900

    :recycle: plugin format

--- AppData/Local/nvim/lua/daiki/packers/init.lua
+++ AppData/Local/nvim/lua/daiki/packers/init.lua
@@ -31,7 +31,6 @@ packer.startup({ function(use)
 	use { 'nvim-lua/popup.nvim' }
 	use { 'nvim-lua/plenary.nvim' }
 	use { 'MunifTanjim/nui.nvim' }
-	-- use { 'vim-denops/denops.vim' }
 
 	-- colorscheme
 	-- use { 'EdenEast/nightfox.nvim' }
@@ -55,12 +54,10 @@ packer.startup({ function(use)
 		'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate',
 	}
-	use { 'yioneko/nvim-yati', requires = 'nvim-treesitter/nvim-treesitter' }
 
 	-- brackets
 	use {
 		'windwp/nvim-autopairs',
-		event = { "InsertEnter" },
 		config = function() require("nvim-autopairs").setup {} end
 	}
 
@@ -75,7 +72,13 @@ packer.startup({ function(use)
 	use { 'terrortylor/nvim-comment' }
 
 	-- LSP
-	use { 'neovim/nvim-lspconfig' }
+	use({
+		'neovim/nvim-lspconfig',
+		event = { "VimEnter" },
+		config = function()
+			require("modules.lspconfig")
+		end
+	})
 	-- use { 'jose-elias-alvarez/null-ls.nvim' }
 	use { 'williamboman/mason.nvim' }
 	use { 'williamboman/mason-lspconfig.nvim' }
@@ -96,7 +99,7 @@ packer.startup({ function(use)
 		end,
 	})
 
-	use { 'onsails/lspkind.nvim' }
+	-- use { 'onsails/lspkind.nvim' }
 	use({
 		'glepnir/lspsaga.nvim',
 		event = { "BufRead" },
@@ -104,22 +107,39 @@ packer.startup({ function(use)
 			require("modules.lspsaga")
 		end
 	})
-	use { 'j-hui/fidget.nvim' }
-	use { 'ray-x/lsp_signature.nvim' }
+	use({
+		'j-hui/fidget.nvim',
+		event = { "BufRead" },
+		config = function()
+			require("modules.fidget")
+		end
+	})
+	use({
+		'ray-x/lsp_signature.nvim',
+		event = { "BufRead" },
+		requires = {
+			{ 'neovim/nvim-lspconfig' }
+		},
+		wants = { 'nvim-lspconfig' },
+		config = function()
+			require("modules.lsp_signature")
+		end,
+	})
 
 	-- skkeleton
 	-- use { 'vim-skk/skkeleton', requires = { 'vim-denops/denops.vim' } }
 	-- use { 'rinx/cmp-skkeleton', after = { 'nvim-cmp', 'skkeleton' } }
 
-	use { 'Daiki48/hello-daiki', opt = true, requires = { 'vim-denops/denops.vim' } }
+	-- use { 'Daiki48/hello-daiki', opt = true, requires = { 'vim-denops/denops.vim' } }
 
 	-- fzf, filer
 	use({
 		'nvim-telescope/telescope.nvim',
 		-- module = { 'telescope' },
+		-- event = { "BufEnter" },
 		requires = {
-			{ 'nvim-telescope/telescope-file-browser.nvim' }
-			-- { 'nvim-telescope/telescope-file-browser.nvim', opt = true }
+			{ 'nvim-telescope/telescope-file-browser.nvim' },
+			{ 'nvim-tree/nvim-web-devicons', opt = true, module = { 'nvim-web-devicons' } },
 		},
 		-- wants = { 'telescope-file-browser.nvim' },
 		-- setup = function()
@@ -158,17 +178,6 @@ packer.startup({ function(use)
 			require("modules.gitsigns")
 		end
 	})
-
-	-- notification
-	-- use {
-	--   'folke/noice.nvim',
-	--   event = "VimEnter",
-	--   requires = {
-	--     'MunifTanjim/nui.nvim',
-	--     'rcarriga/nvim-notify',
-	--     'j-hui/fidget.nvim'
-	--   }
-	-- }
 
 	-- rust
 	use { 'simrat39/rust-tools.nvim', opt = true, ft = { "rust" } }
