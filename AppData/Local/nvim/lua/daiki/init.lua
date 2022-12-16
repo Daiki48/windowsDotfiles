e97171f4169c906520efcc463b9a9f29acdaae44
Daiki48 <daiki48.engineer@gmail.com>
2022-12-16 21:59:07 +0900

    :coffin: comment delete

--- AppData/Local/nvim/lua/daiki/init.lua
+++ AppData/Local/nvim/lua/daiki/init.lua
@@ -1,17 +1,6 @@
 print("To start telescope, first run ff in normal mode.")
 
-
-
 require('daiki.options.disable') -- default plugin disable
 require('daiki.options.base') -- base options
-
--- local development
--- vim.opt.runtimepath:append("D:/projects/vim-plugin-dev/navigation.nvim")
--- vim.opt.runtimepath:append("D:/projects/vim-plugin-dev/coolnessFlair.vim")
-
--- require("navigation")
--- require("coolnessFlair")
-
--- require('plugins.packerconfig') -- plugin manager
 require('daiki.packers')
 require('daiki.options.keymaps') -- keymaps
