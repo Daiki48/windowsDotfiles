3b64c8d00a33b9d50cd3bc5023b33722aca3701e
Daiki48 <daiki48.engineer@gmail.com>
2022-12-18 21:36:30 +0900

    :coffin: Delete code

--- AppData/Local/nvim/lua/daiki/dein/init.lua
+++ AppData/Local/nvim/lua/daiki/dein/init.lua
@@ -12,16 +12,11 @@ if not string.match(vim.o.runtimepath, '/dein.vim') then
 	vim.o.runtimepath = dein_repo_dir .. ',' .. vim.o.runtimepath
 end
 
--- if vim.fn.isdirectory(dein_repo_dir) ~= 1 then
--- 	os.execute('git clone https://github.com/Shougo/dein.vim ' .. dein_repo_dir)
--- end
--- vim.o.runtimepath = dein_repo_dir .. ',' .. vim.o.runtimepath
-
 if vim.call('dein#min#load_state', dein_dir) ~= 1 then
 	return
 end
 
-local dein_toml_dir = vim.env.HOME .. '/AppData/Local/nvim/lua/daiki/dein/toml'
+local dein_toml_dir = vim.env.HOME .. '/AppData/Local/nvim/toml'
 local dein_toml_start = dein_toml_dir .. '/start.toml'
 local dein_toml_lazy = dein_toml_dir .. '/lazy.toml'
 local dein_toml_telescope = dein_toml_dir .. '/telescope.toml'
@@ -39,20 +34,20 @@ vim.call('dein#load_toml', dein_toml_lspconfig, { lazy = 1 })
 vim.call('dein#end')
 vim.call('dein#save_state')
 
--- -- install check
--- if (vim.call('dein#check_install') ~= 0) then
--- 	vim.call('dein#install')
--- end
---
--- -- plugin remove check
--- local removed_plugins = vim.call('dein#check_clean')
--- if vim.fn.len(removed_plugins) > 0 then
--- 	vim.fn.map(removed_plugins, "delete(v:val, 'rf')")
--- 	vim.call('dein#recache_runtimepath')
--- 	local impatient_ok, impatient = pcall(require, "impatient")
--- 	if impatient_ok then
--- 	  impatient.enable_profile()
--- 	end
--- end
+-- install check
+if (vim.call('dein#check_install') ~= 0) then
+	vim.call('dein#install')
+end
+
+-- plugin remove check
+local removed_plugins = vim.call('dein#check_clean')
+if vim.fn.len(removed_plugins) > 0 then
+	vim.fn.map(removed_plugins, "delete(v:val, 'rf')")
+	vim.call('dein#recache_runtimepath')
+	local impatient_ok, impatient = pcall(require, "impatient")
+	if impatient_ok then
+	  impatient.enable_profile()
+	end
+end
 
 
