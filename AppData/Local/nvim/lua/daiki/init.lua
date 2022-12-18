4daf27a025bee693ce11128129b565563abf1283
Daiki48 <daiki48.engineer@gmail.com>
2022-12-18 17:38:44 +0900

    :memo: dein disabled

--- AppData/Local/nvim/lua/daiki/init.lua
+++ AppData/Local/nvim/lua/daiki/init.lua
@@ -1,6 +1,5 @@
-print("To start telescope, first run ff in normal mode.")
-
 require('daiki.options.disable') -- default plugin disable
 require('daiki.options.base') -- base options
+-- require('daiki.dein')
 require('daiki.packers')
 require('daiki.options.keymaps') -- keymaps
