fbbbaa379148d1cf61251de20e6b4ce73be6fcc2
Daiki48 <daiki48.engineer@gmail.com>
2022-12-18 18:13:38 +0900

    :construction: dein active

--- AppData/Local/nvim/lua/daiki/init.lua
+++ AppData/Local/nvim/lua/daiki/init.lua
@@ -1,5 +1,5 @@
 require('daiki.options.disable') -- default plugin disable
 require('daiki.options.base') -- base options
--- require('daiki.dein')
-require('daiki.packers')
+require('daiki.dein')
+-- require('daiki.packers')
 require('daiki.options.keymaps') -- keymaps
