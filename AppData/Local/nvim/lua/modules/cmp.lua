601b615c8a7abecf9d1bba0b04c14dd6abd8efe3
Daiki48 <daiki48.engineer@gmail.com>
2022-12-20 22:22:05 +0900

    :zap: Add plugin "cmp-cmdline"

--- AppData/Local/nvim/lua/modules/cmp.lua
+++ AppData/Local/nvim/lua/modules/cmp.lua
@@ -190,3 +190,26 @@ local options = {
 }
 
 cmp.setup(options)
+
+-- `/` cmdline setup.
+cmp.setup.cmdline('/', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+
+-- `:` cmdline setup.
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' }
+	}, {
+		{
+			name = 'cmdline',
+			option = {
+				ignore_cmds = { 'Man', '!' }
+			}
+		}
+	})
+})
