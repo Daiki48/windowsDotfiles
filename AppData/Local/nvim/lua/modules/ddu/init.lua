525993156c886ee7ff5847b18e8275b739f851e0
Daiki48 <daiki48.engineer@gmail.com>
2022-12-22 17:14:47 +0900

    :truck: Moving config "config.lua"

--- AppData/Local/nvim/lua/modules/ddu/init.lua
+++ AppData/Local/nvim/lua/modules/ddu/init.lua
@@ -1,18 +1,1 @@
-local ddu = {
-	patch_global = vim.fn['ddu#custom#patch_global'];
-}
-
-ddu.patch_global({
-	ui = 'ff',
-	sources = { name = 'file_rec', params = {} },
-	sourceOptions = {
-		_ = {
-			matchers = 'matcher_substring',
-		},
-	},
-	kindOptions = {
-		file = {
-			defaultAction = 'open',
-		},
-	},
-})
+require("modules.ddu.config")
