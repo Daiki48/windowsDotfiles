70768f4550d28bb79025f56808a0b3bdc50d7d45
Daiki48 <daiki48.engineer@gmail.com>
2022-12-23 00:35:11 +0900

    :wrench: Update ddu config

--- AppData/Local/nvim/lua/modules/ddu/config.lua
+++ AppData/Local/nvim/lua/modules/ddu/config.lua
@@ -1,15 +1,22 @@
 local ddu = {
 	patch_global = vim.fn["ddu#custom#patch_global"];
+	alias = vim.fn["ddu#custom#alias"];
 }
+
+ddu.alias("source", "directory_rec", "file_external")
 
 ddu.patch_global({
 	ui = "ff",
 	uiParams = {
 		ff = {
-			displaySourceName = "short",
+			-- displaySourceName = "short",
 			prompt = ">",
 			split = "floating",
 			floatingBorder = "rounded",
+			previewFloating = true,
+			previewFloatingBorder = "rounded",
+			filterSplitDirection = 'floating',
+			startFilter = true,
 		}
 	},
 	-- sources = {
