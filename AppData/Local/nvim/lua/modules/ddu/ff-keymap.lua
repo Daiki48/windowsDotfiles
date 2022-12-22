4167ae13b19489107e94bd20a15189ec2c0ff3b8
Daiki48 <daiki48.engineer@gmail.com>
2022-12-23 00:35:58 +0900

    :zap: Add ddu-ui-ff keymap

--- AppData/Local/nvim/lua/modules/ddu/ff-keymap.lua
+++ AppData/Local/nvim/lua/modules/ddu/ff-keymap.lua
@@ -6,8 +6,17 @@ local ddu = {
 }
 
 M.ddu_ff_setting = function ()
-	vim.keymap.set("n", "<CR>", function() return ddu.do_action("itemAction") end)
-	vim.keymap.set("n", "q", function() return ddu.do_action("quit") end)
+	vim.keymap.set("n", "<CR>", function() return ddu.do_action("itemAction") end, { buffer = 0 })
+	vim.keymap.set("n", "i", function() return ddu.do_action("openFilterWindow") end, { buffer = 0 })
+	vim.keymap.set("n", "q", function() return ddu.do_action("quit") end, { buffer = 0 })
+	vim.keymap.set("n", "<ESC>", function() return ddu.do_action("quit") end, { buffer = 0 })
+end
+
+M.ddu_ff_filter_setting = function ()
+	vim.keymap.set("i", "<CR>", "<ESC><cmd>call ddu#ui#ff#close()<CR>", { buffer = 0 })
+	vim.keymap.set("i", "<ESC>", function() return vim.fn["ddu#ui#ff#close"]() and ddu.do_action("quit") end, { buffer = 0 })
+	vim.keymap.set("n", "<ESC>", function() return vim.fn["ddu#ui#ff#close"]() and ddu.do_action("quit") end, { buffer = 0 })
+	vim.keymap.set("n", "q", function() return vim.fn["ddu#ui#ff#close"]() and ddu.do_action("quit") end, { buffer = 0 })
 end
 
 M.find_files = function ()
@@ -20,9 +29,15 @@ M.find_files = function ()
 	})
 end
 
+
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = {"ddu-ff"},
 	callback = function() return M.ddu_ff_setting() end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {"ddu-ff-filter"},
+	callback = function() return M.ddu_ff_filter_setting() end,
 })
 
 vim.keymap.set("n", "ff", function() return M.find_files() end)
