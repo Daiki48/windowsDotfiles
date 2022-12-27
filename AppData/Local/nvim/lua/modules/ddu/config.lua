c61d5ac4186969ceedf1fd6326ccab508a17893d
Daiki48 <daiki48.engineer@gmail.com>
2022-12-27 23:15:24 +0900

    :zap: Add ddu-ui-ff ^& ddu-ui-filer highlights ^& column icon

--- AppData/Local/nvim/lua/modules/ddu/config.lua
+++ AppData/Local/nvim/lua/modules/ddu/config.lua
@@ -3,6 +3,13 @@ local ddu = {
 	alias = vim.fn["ddu#custom#alias"];
 }
 
+-- local lines = vim.o.lines
+-- local win_height = math.floor(lines / 4)
+-- local win_row = math.floor(lines - win_height)
+-- local win_col = math.floor(vim.o.columns)
+-- local win_between = 3
+-- local win_width = math.floor((vim.o.columns - win_col * 2 - win_between) / 2)
+
 ddu.alias("source", "directory_rec", "file_external")
 
 ddu.patch_global({
@@ -10,7 +17,7 @@ ddu.patch_global({
 	uiParams = {
 		ff = {
 			displaySourceName = "no",
-			prompt = ">",
+			prompt = "> ",
 			split = "floating",
 			floatingBorder = "rounded",
 			previewFloating = true,
@@ -18,12 +25,25 @@ ddu.patch_global({
 			filterSplitDirection = "floating",
 			filterFloatingPosition = "top",
 			startFilter = true,
+			highlights = {
+				floating = "DduUiFfFloating",
+				prompt = "DduUiFfPrompt",
+				selected = "DduUiFfSelected",
+			},
+			-- winRow = win_row,
+			-- winCol = win_col,
+			-- winHeight = win_height - 1,
+
 		},
 		filer = {
 			split = "floating",
 			sort = "filename",
 			sortTreesFirst = true,
 			toggle = true,
+			highlights = {
+				floating = "DduUiFilerFloating",
+				selected = "DduUiFilerSelected"
+			},
 		}
 	},
 	uiOptions = {
@@ -37,6 +57,9 @@ ddu.patch_global({
 		},
 		dein_update = {
 			matchers = {"matcher_dein_update"},
+		},
+		file = {
+			columns = {"icon_filename"},
 		},
 	},
 	kindOptions = {
@@ -57,7 +80,12 @@ ddu.patch_global({
 		echoDiff = {
 			quit = false,
 		},
-	}
+	},
+	filterParams = {
+		matcher_substring = {
+			highlightMatched = "DduUiFfFilterMatch",
+		}
+	},
 })
 
 
