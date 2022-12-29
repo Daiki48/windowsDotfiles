a7ac9fe042465378dc496b90a11d3f0aa2b8eb80
Daiki48 <daiki48.engineer@gmail.com>
2022-12-29 23:02:29 +0900

    :zap: ddc#enable

---
+++ AppData/Local/nvim/lua/modules/ddc/ddc-enable.lua
@@ -0,0 +1,8 @@
+local status = vim.fn["denops#server#status"]();
+
+-- vim.cmd("call denops#server#start()")
+
+-- print("status is " .. status)
+
+vim.call("ddc#enable")
+-- vim.call("popup_preview#enable")
