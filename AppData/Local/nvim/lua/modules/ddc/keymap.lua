b8a3c356a488efb3f266ed813b29eca1510568ac
Daiki48 <daiki48.engineer@gmail.com>
2022-12-29 23:02:52 +0900

    :coffin: commentout

--- AppData/Local/nvim/lua/modules/ddc/keymap.lua
+++ AppData/Local/nvim/lua/modules/ddc/keymap.lua
@@ -1,5 +1,14 @@
 vim.keymap.set("i", "<Tab>", "<cmd>call pum#map#insert_relative(+1)<CR>")
 vim.keymap.set("i", "<S-Tab>", "<cmd>call pum#map#insert_relative(-1)<CR>")
 
-vim.call("ddc#enable")
--- vim.call("popup_preview#enable")
+-- if vim.fn["dein#is_sourced"]("denops-popup-preview") then
+-- 	vim.call("popup_preview#enable")
+-- end
+
+-- if vim.fn["denops#server#start"]() then
+-- 	vim.call("popup_preview#enable")
+-- else
+-- 	vim.call("denops#server#start")
+-- 	vim.call("popup_preview#enable")
+-- end
+
