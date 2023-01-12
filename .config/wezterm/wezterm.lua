1c82f4e6a9b8c92a4355edf1206c53dd42a01627
Daiki48 <daiki48.engineer@gmail.com>
2023-01-13 00:30:24 +0900

    :sparkles: Minimal Base config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require 'wezterm'
+return {
+  default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh' },
+  hide_tab_bar_if_only_one_tab = true,
+  window_background_opacity = 0.8,
+  window_padding = {
+	  left = 0,
+	  right = 0,
+	  top = 0,
+	  bottom = 0,
+  },
+  keys = {
+    { key = 'C', mods = 'CTRL', action = wezterm.action.Copy },
+    { key = 'v', mods = 'SHIFT|CTRL', action = wezterm.action.Paste },
+  },
+}
